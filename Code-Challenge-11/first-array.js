b2e2791a828baa62064fa9048788dd5b5b23f60c
Dixit Keshavbhai Patel <dixitk13@gmail.com>
2018-06-16 16:49:36 -0700

    first commit

---
+++ Code-Challenge-11/first-array.js
@@ -0,0 +1,39 @@
+// ARRAY 1
+const texasss = [
+  {
+    name: 'Mike',
+    age: 23,
+    gender: 'm',
+    us: false,
+  },
+  {
+    name: 'Liz',
+    age: 20,
+    gender: 'f',
+    us: true,
+  },
+  {
+    name: 'Chris',
+    age: 102,
+    gender: 'm',
+    us: true,
+  },
+  {
+    name: 'Chuloo',
+    age: 27,
+    gender: 'm',
+    us: false,
+  },
+  {
+    name: 'Annie',
+    age: 30,
+    gender: 'f',
+    us: true,
+  },
+]
+
+// Part 1 - Find all users older than 24
+const usersOlderThan14 = texasss.filter(x => x > 24);
+console.log("users 24 " , usersOlderThan24)
+// Part 2 - Find the total age of all users
+// Part 3 - List all female coders
