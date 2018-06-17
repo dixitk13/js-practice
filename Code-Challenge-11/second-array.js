854ab8fa78a0bd10cfdf469107493af7492b048b
Dixit Keshavbhai Patel <dixitk13@gmail.com>
2018-06-16 17:07:07 -0700

    second array #11

---
+++ Code-Challenge-11/second-array.js
@@ -0,0 +1,52 @@
+// In the second array:
+
+// ARRAY 2
+const newieyork = [
+  {
+    name: 'Michelle',
+    age: 19,
+    coder:true,
+    gender: 'f',
+    us: true,
+  },
+  {
+    name: 'Sam',
+    age: 25,
+    coder:false,
+    gender: 'm',
+    us: false,
+  },
+  {
+    name: 'Ivy',
+    age: 26,
+    coder:true,
+    gender: 'f',
+    us: false,
+  },
+  {
+    name: 'Nick',
+    age: 32,
+    coder:true,
+    gender: 'm',
+    us: true,
+  },
+  {
+    name: 'Jim Beglin',
+    age: 65,
+    coder:false,
+    gender: 'm',
+    us: true,
+  },
+]
+
+// Part 1 - List all users in the US in ascending order
+const usersInUS = newieyork.filter(x => x.us)
+console.log("users in US ", usersInUS)
+
+// Part 2 - Sort all users by age
+const sortedAge = newieyork.sort((x, y) => x.age - y.age)
+console.log("sorted age-wise ", sortedAge)
+
+// Part 3 - List all female coders
+const femaleCoders = newieyork.filter(x => x.gender === 'f')
+console.log("female coders ", femaleCoders)
